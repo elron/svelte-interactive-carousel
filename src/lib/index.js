2e7b058370fc65e6ecf4c003c6faa5bd7585c71d
Elron <elron100duck@gmail.com>
2022-09-03 08:50:59 +0300

    export

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,3 @@
 // Reexport your entry components here
-export { default } from "./InteractiveCarousel.svelte";
+export { default as InteractiveCarousel } from "./InteractiveCarousel.svelte";
+// export InteractiveCarousel from "./InteractiveCarousel.svelte";
